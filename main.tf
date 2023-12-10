f584cd61ca79c9f7de8df90481b4e41dab950d20
curtisvanausdal <94870086+curtisvanausdal@users.noreply.github.com>
2023-12-10 12:48:06 -0700

    cidr blocks syntanx error

--- main.tf
+++ main.tf
@@ -36,10 +36,10 @@ module "web_sg" {
   vpc_id      = data.aws_vpc.default.id
  
   ingress_rule = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_block = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rule = ["all-all"]
-  egress_cidr_block = ["0.0.0.0/0"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "web" {
